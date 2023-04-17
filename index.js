9e4b764222af2f15eba3e684e0a794efede9d904
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-04-16 23:50:42 -0500

    Some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 const { argv } = require("process")
 
-const starterArg = argv[3] || ""
+const starterArg = argv[2] || ""
 const starters = {}
 
 fs.readdirSync(__dirname+"/tools/").map((path) => {
@@ -19,8 +19,7 @@ fs.readdirSync(__dirname+"/tools/").map((path) => {
 if (starters[starterArg])
 {
     const line = argv.slice(3)
-    console.log(line)
     starters[starterArg](line)
 } else {
-    console.log(`Error: "${starterArg}" is not a valid argument`)
+    console.log(`Error: "${starterArg}" is not a valid command`)
 }
--- tools/helpStr.js
+++ tools/helpStr.js
@@ -1,21 +1,52 @@
 const fs = require("fs")
 
+const tab = "     "
+const quotes = '"'
+
+function wrapList(list, mid, head, tail)
+{
+    let str = ""
+    mid = mid || ", "
+    head = head || ""
+    tail = tail || head || ""
+
+    list.map((value) => {
+        str += `${head}${value}${tail}${mid}`
+    })
+
+    str = str.substring(0, str.length-mid.length)
+    return str
+}
+
 function funct()
 {
     let helpStr = ""
     fs.readdirSync(__dirname).map((path) => {
-        if (!path.match(".*\\.js")) { return; }
-        const data = require(__dirname+"/"+path)
-        helpStr += ` `
+        let data
+        const stats = fs.lstatSync(__dirname+"/"+path)
+        if (stats.isDirectory())
+        data = require(__dirname+"/"+path+"/index.js")
+        else if (stats.isFile() && path.match(".*\\.js"))
+        data = require(__dirname+"/"+path)
+        else
+            return;
+        
+        data.commands.map((command) => {
+            helpStr += `${wrapList(command.starters, quotes)}\n${tab}- ${command.description}`
+        })
     })
-}
 
+    console.log(helpStr)
+}
 
 const commands = [
     {
         funct: funct,
-        flags: {},
+        flags: {
+            "-test-flag": "boby"
+        },
         starters: ["", "help"],
+        name: "help",
         description: "Gives a description of every command"
     
     }
